84be1ccc316572fe0a489137e3986311df804200
Anugrah-dev <anugrahdevansh2006@gmail.com>
2024-07-17 13:16:24 +0530

    .

--- calculator/another method/calculator.js
+++ calculator/another method/calculator.js
@@ -1,2 +1,8 @@
 let calcOutput = '' ;
 document.querySelector('#calc-display').value = calcOutput;
+
+ function checkMe(a){
+    console.log(a);
+    console.log(document.getElementById('item'));
+
+}
